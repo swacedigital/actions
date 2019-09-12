9bff6160e1058641467c2cd3274d3071df52561c
Andreas <a.oshtman@gmail.com>
2019-09-12 07:35:00 +0200

    - Added ecs-deploy action

---
+++ ecs-deploy/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "Logging in to docker registry"
+docker login docker.pkg.github.com -u $INPUT_DEPLOY_USER_NAME -p $INPUT_DEPLOY_USER_TOKEN
+echo "Building docker image"
+docker build . --build-arg NPM_REGISTRY_TOKEN=$INPUT_NPM_REGISTRY_TOKEN -t $INPUT_REPOSITORY_URI:latest
+echo "Pushing docker image"
+docker push $INPUT_REPOSITORY_URI
+echo "Deploying to ECS"
+aws ecs update-service --force-new-deployment --cluster $INPUT_CLUSTER_NAME --service $INPUT_SERVICE_NAME --region $INPUT_ECS_REGION
+echo "Finished deploy"
