a7889d381834d29f5e8e48d2f7cd067edf276467
Andreas <a.oshtman@gmail.com>
2020-01-16 12:45:28 +0100

    Updated sentry action to include deploy of release

--- sentry-release/entrypoint.sh
+++ sentry-release/entrypoint.sh
@@ -6,3 +6,4 @@ export SENTRY_AUTH_TOKEN=$INPUT_SENTRY_AUTH_TOKEN
 sentry-cli releases --org $INPUT_SENTRY_ORG new "$VERSION" --project $INPUT_SENTRY_PROJECT
 sentry-cli releases --org $INPUT_SENTRY_ORG set-commits "$VERSION" --auto
 sentry-cli releases --org $INPUT_SENTRY_ORG finalize $VERSION
+sentry-cli releases --org $INPUT_SENTRY_ORG deploys "$VERSION" new -e $INPUT_SENTRY_ENVIRONMENT
